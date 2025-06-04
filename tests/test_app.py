3582335ea844643f2f976b7cf9397dbc9938367b
Zakaria REGUIEG <98493579+Zvki1@users.noreply.github.com>
2025-06-04 16:56:09 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,7 @@ def test_homepage_status():
     url = os.environ.get("API_URL", "http://localhost:5000") + "/"
     response = requests.get(url)
     assert response.status_code == 200
+
 def test_contact_status():
     url = os.environ.get("API_URL", "http://localhost:5000") + "/contact/"
     response = requests.get(url)
