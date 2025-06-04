7a9f416b00f73f3e8e867d964ce0ab8dfb9edd42
Zakaria REGUIEG <98493579+Zvki1@users.noreply.github.com>
2025-06-04 16:51:22 +0200

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+def test_homepage_status():
+    url = os.environ.get("API_URL", "http://localhost:5000") + "/"
+    response = requests.get(url)
+    assert response.status_code == 200
+def test_contact_status():
+    url = os.environ.get("API_URL", "http://localhost:5000") + "/contact/"
+    response = requests.get(url)
+    assert response.status_code == 200
+
+def test_exercices_status():
+    url = os.environ.get("API_URL", "http://localhost:5000") + "/exercices/"
+    response = requests.get(url)
+    assert response.status_code == 200
